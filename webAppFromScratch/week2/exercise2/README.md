acdb42506b5e1c40d9933e6bef97fd8487517628
LeanderVanBaekel <leander.van.baekel@gmail.com>
2016-02-08 23:52:24 +0100

    update

---
+++ webAppFromScratch/week2/exercise2/README.md
@@ -0,0 +1,4 @@
+# webAppFromScratch
+Minor Everything Web 2016
+
+[Link naar de one page webApp](https://leandervanbaekel.github.io/webAppFromScratch/exercise%205/index.html#home)
---
+++ webAppFromScratch/week2/exercise2/javascript/script.js
@@ -0,0 +1,157 @@
+(function () {
+    'use strict';
+    
+    // making some variables
+    var hidden  = "hidden",
+    	thisPage = window.location.hash,
+    	oldPage = "",
+    	url		= window.location.hash;
+
+    var app = {
+    	init: function () {
+    		sections.hideAll();
+    		routes.routie();
+    	}
+    };
+
+	var routes = {
+
+		routie: function () {
+			routie ({
+				'home': function () {
+
+					var data = {
+						welcome: "hallooo!"
+					}
+
+					Transparency.render(document.getElementById('home'), data);
+					sections.enablePage();
+				},
+
+				'bestPractices': function () {
+					sections.enablePage();
+				},
+
+				'apiData': function () {
+
+					nanoajax.ajax({url:'http://www.colourlovers.com/api/palettes/random'}, function (code, responseText) {
+						var codeVar = code;
+						var responseTextVar = responseText;
+						console.log(codeVar+" "+responseTextVar);
+					});
+
+					var data = {
+						pageTitle: "API Data inladen"
+					}
+
+					Transparency.render(document.getElementById('apiData'), data);
+					sections.enablePage();
+				},
+
+				'*': function () {
+					sections.enablePage();
+				}
+			});
+		}
+
+	};
+
+	var sections = {
+
+		// function called in the app.innit to hide al pages
+		// this is not done in the html because we want the page to be visible if there is no javascript in the browser
+		hideAll: function () {
+
+			
+			var allPages = document.querySelectorAll(".page"); // saving al the pages from the HTML
+
+			for (var i = 0; i < allPages.length; i++) { // loop through the pages
+				allPages[i].classList.add(hidden);
+			};
+
+			if (!window.location.hash) {
+
+			}
+		},
+
+		enablePage: function () {
+
+			var _pageId;
+
+			thisPage = window.location.hash;
+			_pageId = document.querySelector(thisPage);
+
+			
+			if (_pageId) {
+				_pageId.classList.remove(hidden);
+
+			} 
+			else {
+				this.notFound();
+			}
+
+			if (oldPage) {
+				this.disablePage();
+			}
+			oldPage = thisPage;
+
+		},
+
+		// disabeling page by adding hidden class
+		disablePage: function () {
+
+			var _pageId = document.querySelector(oldPage);
+
+			if (_pageId) {
+				_pageId.classList.add(hidden);
+			}
+		},
+
+		// if hash doesn't exist
+		notFound: function () {
+			window.location.hash = 'notFound';
+			this.enablePage();
+		}
+	};
+
+
+	app.init();
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
