bc8dee721344c21f67c828f5b8b0e5db29f3a5df
LeanderVanBaekel <leander.van.baekel@gmail.com>
2016-02-18 22:33:40 +0100

    making modules(?)

--- webAppFromScratch/week3/javascript/loader.js
+++ webAppFromScratch/week3/javascript/loader.js
@@ -1,12 +1,24 @@
 var loader = (function(){
-	return {
-		toggleOn: function () {
-			document.getElementById('loadXhr').classList.remove('hidden');
-			document.getElementById('dataSection').classList.add('hidden');
+	var loaderOb = {
+
+		loaderElement: function () {
+			return document.getElementById('loadXhr');
 		},
+
+		dataElement: function () {
+			return document.getElementById('dataSection');
+		},
+
+		toggleOn: function () {
+			loaderOb.loaderElement().classList.remove('hidden');
+			loaderOb.dataElement().classList.add('hidden');
+		},
+
 		toggleOff: function () {
-			document.getElementById('loadXhr').classList.add('hidden');
-			document.getElementById('dataSection').classList.remove('hidden');
+			loaderOb.loaderElement().classList.add('hidden');
+			loaderOb.dataElement().classList.remove('hidden');
 		}
 	};
+
+	return {toggleOn: loaderOb.toggleOn, toggleOff: loaderOb.toggleOff};
 }());
--- webAppFromScratch/week3_old/README.md
+++
@@ -1,4 +0,0 @@
-# webAppFromScratch
-Minor Everything Web 2016
-
-[Link naar de one page webApp](https://leandervanbaekel.github.io/webAppFromScratch/week3/index.html)
