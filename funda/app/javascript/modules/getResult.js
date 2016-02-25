f7857a1b3fc254b87db53e0be775c8b08a7f29fc
LeanderVanBaekel <leander.van.baekel@gmail.com>
2016-02-25 13:16:38 +0100

    check

--- funda/app/javascript/modules/getResult.js
+++ funda/app/javascript/modules/getResult.js
@@ -50,11 +50,16 @@ var getResult = (function () {
 		searchRequest: function (searchQuery) {
 			dataRequestObj.dataRequest('http://funda.kyrandia.nl/feeds/Aanbod.svc/json/e2d60e885b8742d4b0648300e3703bd7/?type=koop&zo=/', searchQuery, '/&page=1&pagesize=25', function () {
 				templates.render('metadata', requestData.Metadata); // Rendering the data for the page with Transparancy
+				for (var i = 0; i < requestData.Objects.length; i++) {
+					requestData.Objects[i].Prijs.Koopprijs = util.addPrice(requestData.Objects[i].Prijs.Koopprijs);
+				}
 				templates.render('dataSection', requestData.Objects, true);
 			});
 		},
 		objectRequest: function (searchQuery) {
 			dataRequestObj.dataRequest('http://funda.kyrandia.nl/feeds/Aanbod.svc/json/detail/e2d60e885b8742d4b0648300e3703bd7/koop/', searchQuery, null, function () {
+				console.log(requestData);
+				requestData.Prijs.Koopprijs = util.addPrice(requestData.Prijs.Koopprijs);
 				templates.render('object', requestData, true);
 			});
 		}
--- funda/app/javascript/modules/util.js
+++ funda/app/javascript/modules/util.js
@@ -6,12 +6,17 @@ var util = (function () {
 		},
 		getSelector: function (selector) { // simple function to get get element from the DOM
 			return document.querySelector(selector);
+		},
+		addPrice: function (x) {
+    		x = x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    		return x = "€ " + x
 		}
 	};
 
 	return {
 		getId: utilObj.getId, 
-		getSelector: utilObj.getSelector
+		getSelector: utilObj.getSelector,
+		addPrice: utilObj.addPrice
 	};
 
 }());
