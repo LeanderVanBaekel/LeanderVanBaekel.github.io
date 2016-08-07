c277d2d7f54609c7321301b2665e98124fe80a38
Leander van Baekel <leandervanbaekel@activecollective.nl>
2016-08-07 13:41:26 +0200

    tk design

---
+++ TKDesignWorks/script.js
@@ -0,0 +1,59 @@
+
+
+window.onscroll = scroll;
+
+small = false;
+var header = document.querySelector('.mainheader');
+
+
+function scroll () {
+    if (window.pageYOffset >= 80 && small === false) {
+        header.classList.add('small');
+        small = true;
+        console.log(header);
+    } else if (window.pageYOffset < 80 && small === true) {
+        header.classList.remove('small');
+        small = false;
+        console.log(header);
+    }
+
+}
+
+//smooth scroll
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+            }
+        }
+    });
+});
+
+var headlines = ["3D ontwerpen", "Metaal bewerken", "3D animatie", "Krachten simulatie", "Tekeningpakketten", "airflow simulatie"];
+var headlinesCount = 0;
+var designerText = document.querySelector('.designer-text');
+
+
+setInterval(function() {
+  // designerText.innerHTML = "";
+  window.setTimeout(function(){
+    designerText.classList.remove('fade');
+
+    designerText.innerHTML = headlines[headlinesCount];
+    headlinesCount ++;
+    if (headlinesCount == headlines.length) {
+      headlinesCount = 0;
+    }
+  }, 500);
+
+  designerText.classList.add('fade');
+
+}, 5 * 1000);
